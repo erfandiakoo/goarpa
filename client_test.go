7cf58b468d2209f22bda2b6a192d881087183fcd
Erfan Diakoo <diakoo123@gmail.com>
2024-11-14 15:22:01 +0330

    version of package

--- client_test.go
+++ client_test.go
@@ -16,7 +16,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/erfandiakoo/goarpa"
+	"github.com/erfandiakoo/goarpa/v2"
 	"github.com/go-resty/resty/v2"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/exp/rand"
